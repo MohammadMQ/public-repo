d01fec087cac70426b1b9f3855919aeb6b414eee
Mohammad M <momi@queue-it.com>
2020-02-20 14:23:14 +0100

    added demo console app, showing dependency injection

---
+++ Presentation/BarService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    public interface IBarService
+    {
+        void DoSomeRealWork();
+    }
+
+    public class BarService : IBarService
+    {
+        private readonly IFooService _fooService;
+        public BarService(IFooService fooService)
+        {
+            _fooService = fooService;
+        }
+
+        public void DoSomeRealWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _fooService.DoThing(i);
+            }
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+using Presentation;
 
-namespace Presentation
+namespace ConsoleApplication
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //    Uncomment to use the built in container
+            //    var serviceProvider = new ServiceCollection()
+            //        .AddLogging()
+            //        .AddSingleton<IFooService, FooService>()
+            //        .AddSingleton<BarService>()
+            //        .BuildServiceProvider();
+
+            //add the framework services
+            var services = new ServiceCollection()
+                .AddLogging();
+
+            //add structuremp
+            var container = new Container();
+            container.Configure(config =>
+            {
+                // Register stuff in container, using the StructureMap APIs...
+                config.Scan(_ =>
+                {
+                    _.AssemblyContainingType(typeof(Program));
+                    _.WithDefaultConventions();
+                });
+                //Populate the container using the service collection
+                config.Populate(services);
+            });
+
+            var serviceProvider = container.GetInstance<IServiceProvider>();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Debug);
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogDebug("Starting application");
+
+            //do the hard work here
+            var bar = serviceProvider.GetService<IBarService>();
+
+            bar.DoSomeRealWork();
+
+            logger.LogDebug("All done!");
+
         }
     }
 }
